ca6e6eb62bca698f878ec10d2d7313adee3f0e4a
Sidharth.S <sidharths182007@gmail.com>
2025-08-12 13:50:54 +0530

    Add files via upload

---
+++ sum_of_2.cpp
@@ -0,0 +1,16 @@
+//print sum of two numbers
+
+#include <iostream>
+
+int main(){
+using namespace std;
+	int num1,num2,sum;
+	cout << "Enter first number:" ;
+	cin >> num1;
+	cout << "Enter second number:" ;
+	cin >> num2;
+	sum=num1+num2;
+	cout << sum  ;
+	return 0;
+	
+}
