eeea825757840a6a90ffc2cfb2c391b257c82358
Sidharth.S <sidharths182007@gmail.com>
2025-08-12 15:03:59 +0530

    Create greetings.cpp

---
+++ greetings.cpp
@@ -0,0 +1,13 @@
+// input name and print greetings with name;
+#include <iostream>
+
+int main(){
+using namespace std;
+string name; //defining variable
+cout << "Enter the name:";// asking for name as input
+cin>>name;//saving input to variable(name)
+cout<<"Namah Shivaya, "<<name; 
+return 0;
+
+
+}
