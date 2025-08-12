e54fbfc90c7d8a71f4c2a728a6ad685c7af0abb9
Sidharth.S <sidharths182007@gmail.com>
2025-08-12 23:00:33 +0530

    Create triangle_area.cpp

---
+++ triangle_area.cpp
@@ -0,0 +1,17 @@
+//find area of rectangle
+
+#include <iostream>
+using namespace std;
+int main(){
+    float len,bre,area;//defining variable for length,breath,area
+    
+    cout<<"Enter length of rectangle:";
+    cin>>len;
+    cout<<"Enter breadth of rectangle:";
+    cin>>bre;
+    area=len*bre;//calculating area
+    cout<<"Area of rectangle:"<<area;
+    
+    return 0;
+    
+}
