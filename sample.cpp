3e57bed9e11472784798f19da3cfa84dc9d778d6
Sidharth.S <sidharths182007@gmail.com>
2025-08-19 15:10:29 +0530

    Create sample.cpp

---
+++ sample.cpp
@@ -0,0 +1,21 @@
+// Find average of 3 numbers.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num1,num2,num3;
+	float avg;
+	cout << "Enter 1st Number: " ;
+	cin >> num1 ;
+	cout << "Enter 2nd Number: " ;
+	cin >> num2 ;
+	cout << "Enter 3rd Number: " ;
+	cin >> num3 ;
+	avg=(num1+num2+num3)/3;
+	cout << "Average is : " << avg;
+	return 0;
+}
+	
+	
+	
