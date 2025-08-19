a8be318752f11ac6507261ab8a9b54e510c579d8
Sidharth.S <sidharths182007@gmail.com>
2025-08-20 00:18:59 +0530

    Create smallest.cpp

---
+++ smallest.cpp
@@ -0,0 +1,23 @@
+// Find Smallest among 2 numbers
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num1, num2;
+	cout << "Enter 1st Number: " ;
+	cin >> num1 ;
+	cout << "Enter 2nd Number: " ;
+	cin >> num2 ;
+	if (num1 < num2 ) {
+	    cout << "Smallest is : " << num1;
+	}
+	else {
+	    cout << "Smallest is : " << num2;
+	}
+	
+	return 0;
+}
+	
+	
+	
