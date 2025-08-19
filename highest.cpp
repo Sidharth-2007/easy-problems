89384d102b3413fed4f4ae545870acd873b4c280
Sidharth.S <sidharths182007@gmail.com>
2025-08-20 00:16:06 +0530

    Create highest.cpp

---
+++ highest.cpp
@@ -0,0 +1,23 @@
+// Find Highest among 2 numbers
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num1, num2;
+	cout << "Enter 1st Number: " ;
+	cin >> num1 ;
+	cout << "Enter 2nd Number: " ;
+	cin >> num2 ;
+	if (num1 > num2 ) {
+	    cout << "Greatest is : " << num1;
+	}
+	else {
+	    cout << "Highest is : " << num2;
+	}
+	
+	return 0;
+}
+	
+	
+	
