6b2d9186ce0e4839a1989524587b578892894871
Sidharth.S <sidharths182007@gmail.com>
2025-08-20 00:28:10 +0530

    Create square.cpp

---
+++ square.cpp
@@ -0,0 +1,17 @@
+// Find Squares of numbers
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num1,sqr;
+	cout << "Enter the Number: " ;
+	cin >> num1 ;
+	sqr = num1 * num1;
+	cout << "The Square of "<< num1 << " is " << sqr ;
+	
+	return 0;
+}
+	
+	
+	
