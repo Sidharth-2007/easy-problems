8544bd637bed5f1c486b81e4286cda588d1b7c2c
Sidharth.S <sidharths182007@gmail.com>
2025-08-22 22:28:13 +0530

    Add files via upload

---
+++ circle_area.cpp
@@ -0,0 +1,15 @@
+//find the Area of the Circle
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int radi;
+    float area;
+    cout << "Enter the Radius : " ;
+    cin >> radi;
+    area = 3.14 * radi * radi ;
+    cout << "The area of the Circle is : " << area ;
+
+
+}
