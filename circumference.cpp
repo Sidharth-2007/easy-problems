dc57650da5dea7909d7103ea88701f76d2f468cc
Sidharth.S <sidharths182007@gmail.com>
2025-08-22 22:36:15 +0530

    Add files via upload

---
+++ circumference.cpp
@@ -0,0 +1,12 @@
+//Find circumference of a Circle
+
+#include <iostream>
+using namespace std;
+int main() {
+    int radii ;
+    float circ ;
+    cout << "Enter the Raidus :" ;
+    cin >> radii ;
+    circ = 2 * 3.14 * radii ;
+    cout << "Circumference of the circle is : " << circ ;
+}
