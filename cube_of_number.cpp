90849c1894abe7ce0a41f9fa384c79b7d6f8ec9d
Sidharth.S <sidharths182007@gmail.com>
2025-08-22 22:21:01 +0530

    Add files via upload

---
+++ cube_of_number.cpp
@@ -0,0 +1,17 @@
+// Find Cubes of numbers
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num1, cube ;
+	cout << "Enter the Number: " ;
+	cin >> num1 ;
+	cube = num1 * num1 * num1 ;
+	cout << "The Cube of "<< num1 << " is " << cube ;
+	
+	return 0;
+}
+	
+	
+	
