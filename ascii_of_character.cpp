5ce71370b018388c4e2cabfae4430a9da4b263af
Sidharth.S <sidharths182007@gmail.com>
2025-08-26 23:51:45 +0530

    Create ascii_of_character.cpp

---
+++ ascii_of_character.cpp
@@ -0,0 +1,12 @@
+// Print ASCII value of character entered. 
+#include <iostream>
+using namespace std ;
+
+int main() {
+    char ch ;
+    cout << "Enter the Character: " ;
+    cin >> ch ;
+    int ascii = ch ;
+    cout << "The ASCII value for " << ch << " = "<< ascii ;
+    return 0;
+}
