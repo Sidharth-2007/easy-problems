8400b647849c9711d19a47b54b7fb6c2b39615af
Sidharth.S <sidharths182007@gmail.com>
2025-08-27 21:11:42 +0530

    Create sum_of_n_numbers.cpp

---
+++ sum_of_n_numbers.cpp
@@ -0,0 +1,17 @@
+//print sum of n natural number 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0, n ;
+    cout << "Enter the number of terms (n): " ;
+    cin >> n ;
+    
+    for (int i = 0; i <= n ; i++) {
+        sum=sum + i;
+        
+    }
+    cout << "Sum of "<< n << " natural number = "<< sum ;
+    
+    return 0;
+}
