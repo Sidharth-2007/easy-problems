2a21dbb023b2d7af17bcd34f70ba431fc803e5e8
Sidharth.S <sidharths182007@gmail.com>
2025-08-29 21:23:18 +0530

    Create DigitOrNot.cpp

---
+++ DigitOrNot.cpp
@@ -0,0 +1,18 @@
+// Check if a character is an digit or not.
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    if (ch >= '0' && ch <= '9') {
+        cout << ch << " is a digit.";
+        } 
+    else {
+        cout << ch << " is not a digit.";
+        }
+
+    return 0;
+}
