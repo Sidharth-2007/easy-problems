8383dc7252b0267de1a105588a2e319eb7fec30c
Sidharth.S <sidharths182007@gmail.com>
2025-08-29 21:16:16 +0530

    Create AlphabetOrNot.cpp

---
+++ AlphabetOrNot.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    if ((ch >= 'A' && ch <= 'Z') or (ch >= 'a' && ch <= 'z')) {
+        cout << ch << " is an alphabet";
+        }
+    else {
+        cout << ch << " is not an alphabet";
+        }
+
+    return 0;
+}
