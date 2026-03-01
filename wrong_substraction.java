2dfd126178e565145270b234b4098d14d331c678
Sidharth-2007 <sidharths182007@gmail.com>
2026-03-02 00:52:57 +0530

    wrong substractions from codeforce in java and python

---
+++ wrong_substraction.java
@@ -0,0 +1,45 @@
+/*A. Wrong Subtraction
+time limit per test1 second
+memory limit per test256 megabytes
+Little girl Tanya is learning how to decrease a number by one, but she does it wrong with a number consisting of two or more digits. Tanya subtracts one from a number by the following algorithm:
+
+if the last digit of the number is non-zero, she decreases the number by one;
+if the last digit of the number is zero, she divides the number by 10 (i.e. removes the last digit).
+You are given an integer number n
+. Tanya will subtract one from it k
+ times. Your task is to print the result after all k
+ subtractions.
+
+It is guaranteed that the result will be positive integer number.
+
+Input
+The first line of the input contains two integer numbers n
+ and k
+ (2≤n≤109
+, 1≤k≤50
+) — the number from which Tanya will subtract and the number of subtractions correspondingly.
+
+Output
+Print one integer number — the result of the decreasing n
+ by one k
+ times.
+
+It is guaranteed that the result will be positive integer number. */
+import java.util.Scanner;
+
+public class wrong_substraction {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int k = in.nextInt();
+    for (int i=0;i<k;i++){
+        if(n%10==0)
+            n=n/10;
+        
+        else
+            n=n-1;
+        
+    }
+    System.out.println(n);
+}
+}
