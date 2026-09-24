985cccdd909c4666a96bcec35c73dd5a987d96db
unknown <sidharths182007@gmail.com>
2026-09-24 22:38:15 +0530

    valid-parentheses

--- valid-parentheses.py
+++ valid-parentheses.py
@@ -6,23 +6,22 @@ Open brackets must be closed by the same type of brackets.
 Open brackets must be closed in the correct order.
 Every close bracket has a corresponding open bracket of the same type.
 LEET CODE '''
-def chelli(s):
-    dic=["}","]",")"]
-    lst1=["{","[","("]
-
-    str1=""
-    for i in s:
-        if i in lst1:
-            str1+=i
-        else:
-            if len(str1)==0:
-                return "false"
-            elif (lst1[dic.index(i)] == str1[-1] ) :
-                str1=str1[:-1]
+class Solution(object):
+    def isValid(self, s):
+        arr=[]
+        d={"{":"}","[":"]","(":")",}
+        for i in range(len(s)):
+            if (s[i] in "{(["):
+                arr.append(s[i])
             else:
-                return "false"
-                
-    return "true"
-s="{()}[]"
-print(chelli(s))
+                if not arr:
+                    return False
+                elif(d[arr[-1]]==s[i]):
+                    arr.pop()
+                else:
+                    return False
+        if not arr:
+                    return True
+
+
 
